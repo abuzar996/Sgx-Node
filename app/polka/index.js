ee1e58375d8d0db25377a073c18cbe51af84f533
Mohsin Riaz <bahrianboy@gmail.com>
2021-09-21 16:11:49 +0500

    updated to use .env

--- app/polka/index.js
+++ app/polka/index.js
@@ -1,10 +1,12 @@
 const { ApiPromise, WsProvider } = require('@polkadot/api');
 const { types } = require('./types');
-const argv = require('minimist')(process.argv.slice(2));
+require('dotenv').config();
 
 let api = null;
-const provider = new WsProvider(argv.BLOCK_CHAIN_URL || 'wss://dev.chaos.ternoa.com');
+const provider = new WsProvider(process.env.BLOCK_CHAIN_URL);
+console.log('BLOCK_CHAIN_URL',process.env.BLOCK_CHAIN_URL)
 module.exports = async function getChainApiInstance() {
+    
     if (api && api.isConnected) {
         return api;
     } else {
